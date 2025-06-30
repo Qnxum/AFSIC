c34ab4a9f7b9da672117f0fa4273e745f409c6a9
Qnxum <qzi.business@icloud.com>
2025-06-30 20:51:09 +0200

    fix: allow dist and build directories to be tracked

---
+++ dist/assets/index-CxjdUTNT.js
@@ -0,0 +1,1 @@
+import{O as a}from"./features-animation-dq8v2U75.js";import"./main-ETxMdzh7.js";var i=a;export{i as default};
---
+++ dist/assets/src-UW24ZMRV-9pn_HZXG.js
@@ -0,0 +1,1 @@
+import{O as a}from"./features-animation-dq8v2U75.js";import"./main-ETxMdzh7.js";var t=a;export{t as default};
